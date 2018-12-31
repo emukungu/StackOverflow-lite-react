640696209e9576411b3631d921924d7802f243e2
Esther <esther.namusisi@andela.com>
2018-12-31 10:30:00 +0300

    chore: configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: "./src/index.html",
+    filename: "index.html",
+    inject: "body"
+})
+
+const mode = process.NODE_ENV || "production";
+module.exports = {
+    mode,
+	entry: "./src/index.js",
+	output: {
+		path: path.resolve("dist"),
+		filename: "bundle.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+			},
+			{
+				test: [/\.js$/, /\.jsx?$/],
+				exclude: /node_modules/,
+				use: "babel-loader"
+			}
+		]
+    },
+    plugins: [HtmlWebpackPluginConfig]
+};
